2a5a6e0bb7a989fecf193556963730745fcd9ab4
Joshua Wise <joshua@joshuawise.com>
2012-07-23 02:27:55 -0400

    xor-me: Just be a little more ordinary.

--- sw/xor-me.c
+++ sw/xor-me.c
@@ -8,29 +8,6 @@
 
 #define XFAIL(p) do { if (p) { fprintf(stderr, "%s (%s:%d): operation failed: %s\n", __FUNCTION__, __FILE__, __LINE__, #p); abort(); } } while(0)
 
-
-int hex2int(unsigned char c) {
-	switch (c) {
-		case '0': case '1':
-		case '2': case '3':
-		case '4': case '5':
-		case '6': case '7':
-		case '8': case '9':
-			return c - '0';
-		case 'a': case 'b':
-		case 'c': case 'd':
-		case 'e': case 'f':
-			return c - 'a' + 0xA;
-		case 'A': case 'B':
-		case 'C': case 'D':
-		case 'E': case 'F':
-			return c - 'A' + 0xA;
-		default:
-			fprintf(stderr, "bad hex nybble '%c'?\n", c);
-			abort();
-	}
-}
-
 int main(int argc, unsigned char **argv) {
 	int i;
 	unsigned char *key;
@@ -45,14 +22,15 @@ int main(int argc, unsigned char **argv) {
 	
 	XFAIL((fd = open(argv[1], O_RDONLY)) < 0);
 	keylen = lseek64(fd, 0, SEEK_END);
-	key = mmap(NULL, keylen, PROT_READ, MAP_SHARED, fd, 0);
-	XFAIL(key == MAP_FAILED);
+	XFAIL((key = malloc(keylen)) == NULL);
+	lseek64(fd, 0, SEEK_SET);
+	read(fd, key, keylen);
 
 	unsigned char buf[16384];
 	while ((i = read(0, buf, sizeof(buf)))) {
 		int j;
 		for (j = 0; j < i; j++)
-			buf[j] ^= key[(j + keyp) % keylen] ^ 0xFF;
+			buf[j] ^= key[(j + keyp) % keylen];
 		keyp += i;
 		keyp %= keylen;
 		write(1, buf, i);
